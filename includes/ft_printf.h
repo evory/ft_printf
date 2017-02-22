16ecaee1f4cc96604af12811833bc58fef428de6
Bryan BRANDT <bbrandt@e2r5p3.42.fr>
2017-02-22 17:00:14 +0100

    delete

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: bbrandt <bbrandt@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/16 22:49:46 by bbrandt           #+#    #+#             */
-/*   Updated: 2017/02/20 16:33:00 by bbrandt          ###   ########.fr       */
+/*   Updated: 2017/02/22 16:56:48 by bbrandt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,6 @@
 # include <stdlib.h>
 # include "../libft/libft.h"
 
-typedef	struct		s_form
-{
-	char			types;
-	int				flags[5];
-	int				width;
-	int				precision;
-	int				modifier;
-	struct s_form	*next;
-}					t_form;
-
 int					ft_printf(const char *format, ...);
-t_form				*ft_new_form(void);
-int					ft_parser(const char *format, t_form **form);
-void				ft_flags_width_precision(const char *format, t_form **form,
-					int *i);
-void				ft_lsadd(t_form **form, t_form *new);
-void				ft_get_types(const char *format, t_form **form, int *i);
 
 #endif
